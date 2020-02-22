548f8754fa43953f1a2078450243ead83d24dd2e
Brennon Nuckols <brennuck@gmail.com>
2020-02-22 10:40:18 -0700

    staged

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const server = require('./server.js');
 
-const port = process.env.PORT || 1234;
-
-server.listen(port, () => {
-    console.log(`\n* Server Running on http://localhost:${port} *\n`);
+server.listen(1234, () => {
+    console.log('\n* Server Running on http://localhost:1234 *\n');
 });
